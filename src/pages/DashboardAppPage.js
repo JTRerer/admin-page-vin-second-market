5c7549b3855e13bc1fa1c4870558264ec994d4e8
JTRerer <trongan147895@gmail.com>
2023-09-22 13:42:35 +0700

    Update

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -107,26 +107,7 @@ export default function DashboardAppPage() {
                 theme.palette.error.main,
               ]}
             />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chartData={[
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ]}
-            />
-          </Grid>
+          </Grid> 
         </Grid>
       </Container>
     </>
